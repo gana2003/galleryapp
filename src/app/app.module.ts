cef9f0df572bff13ca2214f6285c7debfeca7dd1
gana2003 <meenakshigana0@gmail.com>
2022-07-08 14:12:52 +0530

    gallery cmpltd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GalleryappComponent } from './galleryapp/galleryapp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryappComponent
   ],
   imports: [
     BrowserModule,
