460abce035bfe1bd40b698bd88cb1001355d011e
gana2003 <meenakshigana0@gmail.com>
2022-07-08 16:03:13 +0530

    gallery cmptd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GalleryappComponent } from './galleryapp/galleryapp.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { GalleryappComponent } from './galleryapp/galleryapp.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/service.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiceService {
+
+  constructor(private http:HttpClient) { }
+  galleryapp=()=>{
+    return this.http.get("https://jsonplaceholder.typicode.com/photos")
+  }
+}
